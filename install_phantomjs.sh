76de65690387ed265dcd80fdeda15de06c48e9dd
Metehan Sever <metehaansever@gmail.com>
2024-05-06 16:32:21 +0200

    Update install_phantomjs.sh

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -4,21 +4,6 @@
 # This script must be run as root:
 # sudo sh install_phantomjs.sh
 #
-
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root" 1>&2
-	exit 1
-fi
-
-PHANTOM_VERSION="phantomjs-1.9.8"
-ARCH=$(uname -m)
-
-if ! [ $ARCH = "x86_64" ]; then
-	$ARCH="i686"
-fi
-
-PHANTOM_JS="$PHANTOM_VERSION-linux-$ARCH"
-
 sudo apt-get update
 sudo apt-get install build-essential chrpath libssl-dev libxft-dev -y
 sudo apt-get install libfreetype6 libfreetype6-dev -y
